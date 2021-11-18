f20e91fe4c77a73dc1f26b7cef6434863e956177
Fedor Borshev <fedor@borshev.com>
2021-11-18 22:50:46 +0300

    Support for https

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
 FROM node:16-alpine
 
-RUN apk update && apk --no-cache add dumb-init
+RUN apk update && apk --no-cache add dumb-init openssl
+
+RUN openssl req -x509 -nodes -days 3650 -subj "/C=CA/ST=QC/O=Localhost Administration, Inc./CN=localhost" -addext \
+  "subjectAltName=DNS:localhost" -newkey rsa:2048 -keyout /etc/ssl/private/express-selfsigned.key -out /etc/ssl/certs/express-selfsigned.crt;
 
 ADD . /srv
 WORKDIR /srv
--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 var express = require('express');
-var proxy = require('http-proxy-middleware');
+var fs = require('fs');
+var https = require('https');
+var createProxyMiddleware = require('http-proxy-middleware');
 var morgan = require('morgan');
 
 var app = express();
+
 app.use(morgan('combined'));
-app.use(
-    '/',
-    proxy({
+
+app.use('/', createProxyMiddleware({
         target: process.env.TARGET,
         changeOrigin: true,
         followRedirects: true,
-    }),
-);
-app.listen(3000);
+    })
+)
+
+const port = process.env.PORT || 3000;
+
+if (process.env.SELF_SIGNED_TLS) {
+    var key = fs.readFileSync( '/etc/ssl/private/express-selfsigned.key' );
+    var cert = fs.readFileSync( '/etc/ssl/certs/express-selfsigned.crt' );
+
+    https.createServer({key, cert}, app).listen(port);
+    console.log(`[HTTPS] Listening on 0.0.0.0:${port}...`);
+    console.log('Google chrome users, to allow https on localhost, go to chrome://flags/#allow-insecure-localhost');
+} else {
+    app.listen(port);
+    console.log(`Listening on 0.0.0.0:${port}...`);
+}
